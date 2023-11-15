32e67fece83ab9b78dc009865f81c847fdddab20
NSRBSG <nsrbsg@gmail.com>
2023-11-15 18:54:28 +0900

    chore: vercel analytics 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -55,6 +56,7 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
+            <Analytics />
           </div>
         </div>
       </body>
